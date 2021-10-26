2f05f57944d498dc37b2ec5503df297818b6adc1
ManaPro01 <38740012+ManaPro01@users.noreply.github.com>
2021-10-26 16:21:18 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    
+    
+    
+    <head>
+        <meta charset="UTF-8">
+        <title>Aprende en casa</title>  
+    </head>
+    
+    
+    <body bgcolor = "cyan">
+        <CENTER>
+        <TABLE width = "300" height = "200">
+            <TR><td> <img src="titulo.gif"> </td></tr>            
+        </TABLE>
+        </CENTER>
+    
+    
+        <font face ="arial black" size ="5" color = "black">
+        <h1><center>TU PROFE EN CASA</center></h1>
+        
+        
+         <font face ="arial" size ="3" color = "black">
+        <h1><center>ELIGE TU CURSO</center></h1>
+        
+        
+        
+        <CENTER>
+        <TABLE BORDER ="2">
+            <!--primera fila    ALGEBRA-->
+            
+            <TR>
+                <!--celda 1-->
+                
+                <td width="200" height ="200" bgcolor = "navajowhite"> 
+                    <font face ="arial black" size ="3" color = "black">
+                    
+                    <center>ALGEBRA</center>
+                    
+                    <img src="2343501.jpg" alt="Image" height="150" width="300">  <!--IMAGEN-->
+                    
+                    <CENTER> <input type="submit" value="ALGEBRA" name="btnAlgebra" /> </CENTER></td><!--BOTON-->
+    
+                <!--celda 2-->
+                <td width="300" height ="200" bgcolor = "greenyellow"> 
+                    <font face ="arial black" size ="3" color = "black">
+                    <CENTER>
+                     OBJETIVO GENERAL <br>
+                     </CENTER>
+                     <font face ="arial" size ="3" color = "black">
+                     Proporcionar elementos indispensables para el estudio de modelos lineales <br> 
+                     presentando la teoría del álgebra de matrices, formas cuadráticas y sistemas<br>
+                     de ecuaciones lineales. Introducir el concepto de espacios vectoriales para <br>
+                     desarrollar la intuición geométrica que será de gran ayuda para visualizar <br>
+                     y comprender la estructura de los procedimientos del análisis multivariante.</td>
+                     
+                <!--celda 3-->
+                <td width="300" height ="200" bgcolor = "navajowhite">
+                    <font face ="arial black" size ="3" color = "black">
+                    <CENTER>
+                    OBJETIVOS ESPECÍFICOS <br>
+                    </CENTER>
+                    <font face ="arial" size ="3" color = "black">
+                    - Ubicar en lo general la problemática de la docencia en matemáticas, <br>
+                    sus componentes y sus manifestaciones en la práctica educativa.<br>
+                    - Explorar y analizar creencias propias sobre la naturaleza de<br>
+                    las matemáticas, de su enseñanza y sus propósitos,  y reconocer<br>
+                    la influencia que tales creencias podrían tener en la planificación<br>
+                    y gestión de la labor docente en el aula. 
+                    
+                </td>
+            </tr> 
+            
+            
+            
+            
+            
+            <!--segunda fila    ARITMETICA-->
+            <TR><td width="300" height ="200" bgcolor = "navajowhite">
+                     <font face ="arial black" size ="3" color = "black">
+                    
+                    <center>ARITMETICA</center>
+                    
+                    <img src="aritmetica.jpg" alt="Image" height="150" width="300">  <!--IMAGEN-->
+                   <CENTER><input type="submit" value="ARITMETICA" name="btnArit" /> </CENTER>
+                   </td>
+                   
+                   <td width="300" height ="200" bgcolor = "greenyellow">
+                       <font face ="arial black" size ="3" color = "black">
+                   <CENTER>
+                    OBJETIVO GENERAL <br>
+                    </CENTER>
+                   <font face ="arial" size ="3" color = "black">
+                    Dedicarse al estudio de los números y sus propiedades bajo las <br> 
+                    operaciones de suma, resta, multiplicación y división. </td>
+                    
+                    
+                    <td width="300" height ="200" bgcolor = "navajowhite">
+                    <font face ="arial black" size ="3" color = "black">
+                    <CENTER>
+                    OBJETIVOS ESPECÍFICOS <br>
+                    </CENTER>
+                    <font face ="arial" size ="3" color = "black">
+                    - Demostrar habilidad en la ejecución de operaciones aritméticas.<br>
+                    - Utilizar los conocimientos sobre operaciones aritméticas en la <br>
+                    resolución de problemas.<br>
+                    - Clasificar un número dado entro del campo de los números complejos.<br>
+                    - Resolver problemas aritméticos utilizando: suma, resta, <br>
+                    multiplicación, división y potenciación de números racionales;<br> 
+                    jerarquía de operaciones; suma,resta, multiplicación, división <br>
+                    y potenciación de números irracionales.<br>
+                    - Resolver problemas de variación directa e inversa. <br>
+                    - Identificar sucesiones aritméticas y geométricas e identificar <br>
+                    patrones.  
+                    
+                </td>
+                   
+            </tr>
+            
+            
+            
+            
+            
+            
+            <!--tercera fila    GEOMETRIA-->
+            <TR><td width="300" height ="200" bgcolor = "navajowhite"> 
+                    <font face ="arial black" size ="3" color = "black">
+                    
+                    <center>GEOMETRIA</center>
+                    
+                    <img src="geometria.jpg" alt="Image" height="150" width="300">
+                     
+                   <CENTER> <input type="submit" value="GEOMETRIA" name="btnGeom" /> </td></CENTER>
+                   
+                    <td width="300" height ="200" bgcolor = "greenyellow">
+                       <font face ="arial black" size ="3" color = "black">
+                   <CENTER>
+                    OBJETIVO GENERAL <br>
+                    </CENTER>
+                    <font face ="arial" size ="3" color = "black">
+                    Describir, clasificar y estudiar las propiedades de las figuras <br>
+                    geométricas.
+                    
+                    
+                    <td width="300" height ="200" bgcolor = "navajowhite">
+                    <font face ="arial black" size ="3" color = "black">
+                    <CENTER>
+                    OBJETIVOS ESPECÍFICOS <br>
+                    </CENTER>
+                    <font face ="arial" size ="3" color = "black">
+                   - Interiorizar ciertas figuras geométricas, cuadrado, triángulo, <br>
+                   círculo y rectángulo, y sus correspondientes nombres.  <br>
+                   - Reconocer  y  representar  las  formas  geométricas,  cuadrado,<br>
+                   triángulo,  círculo  y rectángulo. <br>
+                   - Clasificar las figuras geométricas según su tamaño, color, <br>
+                   grosor y textura.  <br>
+                   - Fomentar las capacidades de observación, representación, <br>
+                   manipulación, experimentación y discriminación.  <br>
+                   - Relacionar la geometría con aspectos de nuestra vida cotidiana.  
+                   - Desarrollar la creatividad a través de los aprendizajes matemáticos.  
+                   - Ampliar el vocabulario matemático.  
+                   - Fomentar la expresión artística.  
+                   - Tener interés en participar en las diversas actividades. 
+                   - Desarrollar el trabajo colaborativo y adquirir pautas elementales<br>
+                   de convivencia.  <br>
+                   - Adquirir progresivamente una mayor autonomía en diferentes tareas.<br>  
+                   - Fomentar la relación familia-escuela. 
+                    
+                </td>
+            </tr>
+            
+            
+            
+            
+            
+            
+            
+            <!--cuarta fila     CALCULO DIFERENCIAL-->
+            <TR><td width="300" height ="200" bgcolor = "navajowhite"> 
+                     <font face ="arial black" size ="3" color = "black">
+                    
+                    <center>CALCULO DIFERENCIAL</center>
+                    
+                    <img src="calculo diferencial.jpg" alt="Image" height="150" width="300">
+                    <CENTER> <input type="submit" value="CALCULO DIFERENCIAL" name="btnCD" /> </CENTER> </td>
+        
+                     <td width="300" height ="200"  bgcolor = "greenyellow">
+                       <font face ="arial black" size ="3" color = "black">
+                   <CENTER>
+                    OBJETIVO GENERAL <br>
+                    </CENTER>
+                    <font face ="arial" size ="3" color = "black">
+                    El estudiante, con el estudio del Calculo Diferencial,<br>
+                    estará en capacidad de aplicar a modelos matemáticos y <br>
+                    físicos los teoremas, reglas y algoritmos de la derivada<br>
+                    y a la optimización de problemas de la profesión.
+                    
+                    
+                    <td width="300" height ="200" bgcolor = "navajowhite">
+                    <font face ="arial black" size ="3" color = "black">
+                    <CENTER>
+                    OBJETIVOS ESPECÍFICOS <br>
+                    </CENTER>
+                    <font face ="arial" size ="3" color = "black">
+                    - Comprender y aplicar los conceptos fundamentales del <br>
+                      cálculo en una variable. <br>
+                    - Demostrar habilidad en el cálculo de límites y derivadas. <br>
+                    - Representar matemáticamente una situación mediante funciones,<br>
+                      representar gráficamente una función y leer representaciones <br>
+                      gráficas y de funciones. <br>
+                    - Leer, comprender, plantear y proponer alternativas de solución<br>
+                      de problemas de razón de cambio, razón de cambio relacionada y <br>
+                      optimización. <br>
+                    - Promover el uso de los métodos y las aplicaciones del cálculo <br>
+                      en una variable; el desarrollo de pensamiento matemático y la<br>
+                      comunicación de las ideas en un lenguaje matemático. 
+                </td>
+            </tr>
+            
+            
+            <!--quinta fila     CALCULO INTEGRAL-->
+            <TR><td width="300" height ="200" bgcolor = "navajowhite"> 
+                    <font face ="arial black" size ="3" color = "black">
+                    
+                    <center>CALCULO INTEGRAL</center>
+                    <img src="calculo integral.jpg" alt="Image" height="150" width="300">
+                    <CENTER><input type="submit" value="CALCULO INTEGRAL" name="btnCI" /> </CENTER> </td>
+        
+                <td width="300" height ="200" bgcolor = "greenyellow">
+                    <font face ="arial black" size ="3" color = "black">
+                   <CENTER>
+                    OBJETIVO GENERAL <br>
+                    </CENTER>
+                
+                    <font face ="arial" size ="3" color = "black">
+                    Comprender el concepto de integral de una función, manejar los<br>
+                    métodos de integración y aplicarlos a cálculos geométricos y a<br>
+                    problemas de Física, Química, Economía e Ingeniería.
+                    
+                    
+                    <td width="300" height ="200" bgcolor = "navajowhite">
+                    <font face ="arial black" size ="3" color = "black">
+                    <CENTER>
+                    OBJETIVOS ESPECÍFICOS <br>
+                    </CENTER>
+                    <font face ="arial" size ="3" color = "black">
+                    - Conocer y manejar los métodos de integración. <br>
+                    - Calcular, usando integrales, áreas de superficies planas <br>
+                    y de revolución,volúmenes  de  sólidos  de  revolución, <br> 
+                    longitud  de  un  arco,  masa,  centro  de masas, momento <br>
+                    estático y de inercia de una lámina plana, el trabajo realizado <br>
+                    por una fuerza. <br>
+                    - Conocer  y  aplicar  criterios  de  convergencia  para  <br>
+                    calcular sumas de series y desarrollar funciones en series de <br>
+                    potencias. <br>
+                    - Representar geométricamente rectas, planos y superficies <br>
+                    y determinar sus propiedades de intersecciones, simetrías y <br>
+                    extensión. <br>
+                    - Conocer y aplicar las operaciones con vectores del plano y <br>
+                    del espacio. <br>
+                    - Calcular el límite de una función escalar y analizar su <br>
+                    continuidad en un punto.  <br>
+                    - Operar con límites y funciones continuas. <br>
+                    - Calcular e interpretar las derivadas parciales y  <br>
+                    direccionales la diferencial y el gradiente de una función escalar. <br>
+                    - Calcular e interpretar el valor de una integral <br>
+                    iterada  de  una  función  escalar.  <br>
+                    - Invertir el orden de integración. Calcular áreas y volúmenes por  <br>
+                    medio de integración iterada. <br>
+                </td>
+            </tr>
+        </TABLE>
+        </center>
+          
+        
+        <?php
+        // put your code here
+       
+        ?>
+    </body>
+</html>
